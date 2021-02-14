d4dd2fb125d4ef8ee121923b1bfd40aa87afc69f
Jason Flax <jsflax@gmail.com>
2021-02-14 00:58:16 -0500

    switch to gnucxx

--- Package.swift
+++ Package.swift
@@ -244,5 +244,5 @@ let package = Package(
                 .headerSearchPath("../../../external/catch/single_include")
             ] + cxxSettings) as [CXXSetting])
     ],
-    cxxLanguageStandard: .cxx1z
+    cxxLanguageStandard: .gnucxx1z
 )
